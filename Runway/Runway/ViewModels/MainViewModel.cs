ccd9f3573f585293cf18593ce2acfff8a4d51aa1
Alex Novak <alexwnovak@gmail.com>
2016-09-23 12:22:33 -0400

    +CurrentCommandText property, bound to the input field

--- Runway/Runway/ViewModels/MainViewModel.cs
+++ Runway/Runway/ViewModels/MainViewModel.cs
@@ -4,5 +4,17 @@ namespace Runway.ViewModels
 {
    public class MainViewModel : ViewModelBase
    {
+      private string _currentCommandText;
+      public string CurrentCommandText
+      {
+         get
+         {
+            return _currentCommandText;
+         }
+         set
+         {
+            Set( () => CurrentCommandText, ref _currentCommandText, value );
+         }
+      }
    }
 }
