205fc2af5300ae799ef3e57dedbcd2bd2e0a31a1
Alex Novak <alexwnovak@gmail.com>
2016-09-24 13:55:05 -0400

    Fleshing out CommandCatalog to have a few hard-coded commands
    This should depend on some kind of command provider or repository or something, instead of being hard-coded. Eventually this will be supplied a plugin manager.

--- Runway/Runway/CommandCatalog.cs
+++ Runway/Runway/CommandCatalog.cs
@@ -1,10 +1,29 @@
+using System;
+using System.Linq;
+
 namespace Runway
 {
    public class CommandCatalog : ICommandCatalog
    {
+      private static readonly string[] _commandList = CreateCommandList();
+
+      private static string[] CreateCommandList()
+      {
+         return new[]
+         {
+            "copy",
+            "uninstall"
+         };
+      }
+
       public string Resolve( string commandPartialText )
       {
-         return "copy";
+         if ( string.IsNullOrEmpty( commandPartialText ) )
+         {
+            return null;
+         }
+
+         return _commandList.FirstOrDefault( c => c.StartsWith( commandPartialText, StringComparison.InvariantCultureIgnoreCase ) );
       }
    }
 }
