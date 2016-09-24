f02a28384060f6e17bee554d1d7e6c505b34acb2
Alex Novak <alexwnovak@gmail.com>
2016-09-24 14:31:46 -0400

    +CopyLaunchCommand empty implementation
    This will be the guy that copies stuff to the clipboard.

---
+++ Runway/Runway/CopyLaunchCommand.cs
@@ -0,0 +1,14 @@
+namespace Runway
+{
+   public class CopyLaunchCommand : LaunchableCommandBase
+   {
+      public CopyLaunchCommand() : base( "copy" )
+      {
+      }
+
+      public override void Launch( object[] parameters )
+      {
+         
+      }
+   }
+}
