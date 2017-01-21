6a6dfdbdb8b615e935f3d05aaaabaf45222fcec9
Alex Novak <alexwnovak@gmail.com>
2017-01-21 17:05:46 -0500

    +MoveCaret method that operates on a CaretPosition enum

--- Runway/Runway.UnitTests/Views/TextBoxExtensionsTests.cs
+++ Runway/Runway.UnitTests/Views/TextBoxExtensionsTests.cs
@@ -1,6 +1,51 @@
+﻿using System.Windows.Controls;
+using Xunit;
+using FluentAssertions;
+using Runway.ViewModels;
+using Runway.Views;
+
 namespace Runway.UnitTests.Views
 {
    public class TextBoxExtensionsTests
    {
+      [StaFact]
+      public void MoveCaret_PassesStartPosition_SelectionStartShouldBe0()
+      {
+         // Arrange
+
+         var textBox = new TextBox
+         {
+            Text = "Some text",
+            SelectionStart = 2
+         };
+
+         // Act
+
+         textBox.MoveCaret( CaretPosition.Start );
+
+         // Assert
+
+         textBox.SelectionStart.Should().Be( 0 );
+      }
+
+      [StaFact]
+      public void MoveCaret_PassesEndPosition_SelectionStartShouldBeAtTheEnd()
+      {
+         // Arrange
+
+         var textBox = new TextBox
+         {
+            Text = "Some text",
+            SelectionStart = 2
+         };
+
+         // Act
+
+         textBox.MoveCaret( CaretPosition.End );
+
+         // Assert
+
+         textBox.SelectionStart.Should().Be( textBox.Text.Length );
+      }
    }
 }
