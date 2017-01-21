f4f91d08746e12facb6ea90b7c5bd1462914f317
Alex Novak <alexwnovak@gmail.com>
2017-01-21 16:33:00 -0500

    +MoveCaretEventArgs that carries a CaretPosition
    We'll couple this with an event so the view model can request that the view move the caret, without having to do any WPF stuff itself.

---
+++ Runway/Runway/ViewModels/MoveCaretEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Runway.ViewModels
+{
+   public class MoveCaretEventArgs : EventArgs
+   {
+      public CaretPosition CaretPosition
+      {
+         get;
+      }
+
+      public MoveCaretEventArgs( CaretPosition caretPosition )
+      {
+         CaretPosition = caretPosition;
+      }
+   }
+}
