f2104c3431226953f8507a25c101cb9324b8a6a4
Alex Novak <alexwnovak@gmail.com>
2017-01-22 09:58:19 -0500

    +Test for returning null when the command doesn't exist
    Looks trivial. Feeling out how Git and TDD work together.

--- Runway/Runway.UnitTests/ViewModels/CommandParserTests.cs
+++ Runway/Runway.UnitTests/ViewModels/CommandParserTests.cs
@@ -1,6 +1,28 @@
+﻿using FluentAssertions;
+using Moq;
+using Xunit;
+using Runway.ViewModels;
+
 namespace Runway.UnitTests.ViewModels
 {
    public class CommandParserTests
    {
+      [Fact]
+      public void GetCommandSuggestion_CommandDoesNotExist_ReturnsNullSuggestion()
+      {
+         // Arrange
+
+         var commandCatalogMock = new Mock<ICommandCatalog>();
+
+         // Act
+
+         var commandParser = new CommandParser( commandCatalogMock.Object );
+
+         string suggestion = commandParser.GetCommandSuggestion( "doesnotexist" );
+
+         // Assert
+
+         suggestion.Should().BeNull();
+      }
    }
 }
--- Runway/Runway/ViewModels/CommandParser.cs
+++ Runway/Runway/ViewModels/CommandParser.cs
@@ -2,5 +2,16 @@
 {
    public class CommandParser
    {
+      private readonly ICommandCatalog _commandCatalog;
+
+      public CommandParser( ICommandCatalog commandCatalog )
+      {
+         _commandCatalog = commandCatalog;
+      }
+
+      public string GetCommandSuggestion( string partialCommandText )
+      {
+         return null;
+      }
    }
 }
