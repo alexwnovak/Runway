7949b1cd4e50898eb69704c72dc043ae98784675
Alex Novak <alexwnovak@gmail.com>
2017-01-22 10:59:04 -0500

    +A few more test cases for CommandCatalog
    - Finds a command by partial text
    - Returns MissingCommand on null text
    - Returns MissingCommand when a real text search finds nothing

--- Runway/Runway.UnitTests/CommandCatalogTests.cs
+++ Runway/Runway.UnitTests/CommandCatalogTests.cs
@@ -28,5 +28,57 @@ namespace Runway.UnitTests
 
          resolvedCommand.Should().Be( commandMock.Object );
       }
+
+      [Fact]
+      public void Resolve_CommandExists_CommandIsFoundByPartialNameMatch()
+      {
+         const string partialCommandText = "c";
+         const string commandText = "copy";
+
+         // Arrange
+
+         var commandMock = new Mock<ILaunchableCommand>();
+         commandMock.SetupGet( c => c.CommandText ).Returns( commandText );
+
+         // Act
+
+         var commandCatalog = new CommandCatalog();
+
+         commandCatalog.Add( commandMock.Object );
+
+         var resolvedCommand = commandCatalog.Resolve( partialCommandText );
+
+         // Assert
+
+         resolvedCommand.Should().Be( commandMock.Object );
+      }
+
+      [Fact]
+      public void Resolve_PassesNullText_ReturnsMissingCommand()
+      {
+         // Act
+
+         var commandCatalog = new CommandCatalog();
+
+         var command = commandCatalog.Resolve( null );
+
+         // Assert
+
+         command.Should().Be( CommandCatalog.MissingCommand );
+      }
+
+      [Fact]
+      public void Resolve_SpecifiedCommandDoesNotExist_ReturnsMissingCommand()
+      {
+         // Act
+
+         var commandCatalog = new CommandCatalog();
+
+         var command = commandCatalog.Resolve( "doesnotexist" );
+
+         // Assert
+
+         command.Should().Be( CommandCatalog.MissingCommand );
+      }
    }
 }
