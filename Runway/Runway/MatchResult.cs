1d15b9f067119aca31580dfd4137f331ba12fda5
Alex Novak <alexwnovak@gmail.com>
2017-01-25 20:23:09 -0500

    +MatchResult to describe a command and how it was matched

---
+++ Runway/Runway/MatchResult.cs
@@ -0,0 +1,21 @@
+﻿namespace Runway
+{
+   public class MatchResult
+   {
+      public MatchType MatchType
+      {
+         get;
+      }
+
+      public ILaunchableCommand Command
+      {
+         get;
+      }
+
+      public MatchResult( MatchType matchType, ILaunchableCommand command )
+      {
+         MatchType = matchType;
+         Command = command;
+      }
+   }
+}
