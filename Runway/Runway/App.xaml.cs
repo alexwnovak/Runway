ed85035aadc32ffc991876b3e574798699d61348
Alex Novak <alexwnovak@gmail.com>
2017-01-28 10:41:16 -0500

    Adding uninstall to the command catalog on launch

--- Runway/Runway/App.xaml.cs
+++ Runway/Runway/App.xaml.cs
@@ -22,6 +22,7 @@ namespace Runway
       {
          var commandCatalog = new CommandCatalog();
          commandCatalog.Add( new CopyLaunchCommand() );
+         commandCatalog.Add( new UninstallCommand() );
 
          SimpleIoc.Default.Register<ICommandCatalog>( () => commandCatalog );
          SimpleIoc.Default.Register<IAppService>( () => new AppService() );
