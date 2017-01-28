705fb959962c1ffb3b4791c4262fb0218941f235
Alex Novak <alexwnovak@gmail.com>
2017-01-28 10:54:38 -0500

    Behavior now moves the window to a nice spot on load

--- Runway/Runway/Behaviors/WindowPlacementBehavior.cs
+++ Runway/Runway/Behaviors/WindowPlacementBehavior.cs
@@ -5,5 +5,14 @@ namespace Runway.Behaviors
 {
    public class WindowPlacementBehavior : Behavior<Window>
    {
+      protected override void OnAttached() => AssociatedObject.Loaded += AssociatedObject_OnLoaded;
+      protected override void OnDetaching() => AssociatedObject.Loaded -= AssociatedObject_OnLoaded;
+
+      private void AssociatedObject_OnLoaded( object sender, RoutedEventArgs e )
+      {
+         AssociatedObject.WindowStartupLocation = WindowStartupLocation.Manual;
+         AssociatedObject.Left = ( SystemParameters.FullPrimaryScreenWidth - AssociatedObject.Width ) / 2;
+         AssociatedObject.Top = SystemParameters.FullPrimaryScreenHeight / 4;
+      }
    }
 }
