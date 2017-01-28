7a09514de286af756069e7b676406a8803c6b1d0
Alex Novak <alexwnovak@gmail.com>
2017-01-28 17:04:54 -0500

    +Reset for transforming the collection into a new set with one notify

--- Runway/Runway.UnitTests/ViewModels/BulkObservableCollectionTests.cs
+++ Runway/Runway.UnitTests/ViewModels/BulkObservableCollectionTests.cs
@@ -1,6 +1,27 @@
-﻿namespace Runway.UnitTests.ViewModels
+﻿using System.Linq;
+using Xunit;
+using FluentAssertions;
+using Runway.ViewModels;
+
+namespace Runway.UnitTests.ViewModels
 {
    public class BulkObservableCollectionTests
    {
+      [Fact]
+      public void Reset_ResettingToOneItem_OnlyReceivesOneNotification()
+      {
+         // Act
+
+         var collection = new BulkObservableCollection<int>();
+
+         int eventsRaised = 0;
+         collection.CollectionChanged += ( sender, e ) => eventsRaised++;
+
+         collection.Reset( Enumerable.Range( 1, 1 ) );
+
+         // Assert
+
+         eventsRaised.Should().Be( 1 );
+      }
    }
 }
