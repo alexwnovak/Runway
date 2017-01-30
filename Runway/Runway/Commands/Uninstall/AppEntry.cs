86d9a44024782e77bcb9fe6b956930ca6a89332e
Alex Novak <alexwnovak@gmail.com>
2017-01-30 18:38:31 -0500

    +AppEntry class
    This will be what the app catalog deals in. When it finds stuff, it'll return it through this--a lightweight model. When uninstalling, you hand this back to the catalog, so you have to lookup first, instead of looking up IDs and then pushing IDs back.

---
+++ Runway/Runway/Commands/Uninstall/AppEntry.cs
@@ -0,0 +1,21 @@
+﻿namespace Runway.Commands.Uninstall
+{
+   public class AppEntry
+   {
+      public string Name
+      {
+         get;
+      }
+
+      public string Id
+      {
+         get;
+      }
+
+      public AppEntry( string name, string id )
+      {
+         Name = name;
+         Id = id;
+      }
+   }
+}
