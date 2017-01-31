d7b5a053eecfc9cc07ebc17020e3a387783867ee
Alex Novak <alexwnovak@gmail.com>
2017-01-30 19:18:41 -0500

    +IRegistry
    This is a semi-thin layer over the registry. It's not a granular method wrapping idea, but instead it aggregates a little functionality over it, to make the calls a little easier.

---
+++ Runway/Runway/Commands/Uninstall/IRegistry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Runway.Commands.Uninstall
+{
+   public interface IRegistry
+   {
+      string[] GetSubKeysFromLocalMachine( string path );
+
+      Tuple<string, string> GetValuesFromLocalMachine( string path, string key1, string key2 );
+   }
+}
