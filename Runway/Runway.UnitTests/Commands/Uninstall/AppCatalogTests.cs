c9109efdb454d947f027668b46ef7b96bfcc6fb4
Alex Novak <alexwnovak@gmail.com>
2017-01-30 19:20:00 -0500

    AppCatalog now brokers with the IRegistry to find apps

--- Runway/Runway.UnitTests/Commands/Uninstall/AppCatalogTests.cs
+++ Runway/Runway.UnitTests/Commands/Uninstall/AppCatalogTests.cs
@@ -1,6 +1,9 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 using FluentAssertions;
+using Moq;
 using Runway.Commands.Uninstall;
+using Runway.UnitTests.Helpers;
 
 namespace Runway.UnitTests.Commands.Uninstall
 {
@@ -9,15 +12,48 @@ namespace Runway.UnitTests.Commands.Uninstall
       [Fact]
       public void Find_CatalogIsEmpty_ReturnsNoResults()
       {
+         // Arrange
+
+         var registryMock = new Mock<IRegistry>();
+
          // Act
 
-         var appCatalog = new AppCatalog();
+         var appCatalog = new AppCatalog( registryMock.Object );
 
          var results = appCatalog.Find( "notepad" );
 
          // Assert
 
          results.Should().HaveCount( 0 );
+      }
+
+      [Fact]
+      public void Find_SearchesForAppByName_FindsApp()
+      {
+         const string appName = "notepad";
+         const string uninstallString = "uninstall string";
+         const string id = "AppId";
+         
+         // Arrange
+
+         var registryResults = ArrayHelper.Create( id );
+
+         var registryMock = new Mock<IRegistry>();
+         registryMock.Setup( r => r.GetSubKeysFromLocalMachine( It.IsAny<string>() ) ).Returns( registryResults );
+
+         var values = new Tuple<string, string>( appName, uninstallString );
+         registryMock.Setup( r => r.GetValuesFromLocalMachine( id, "DisplayName", "UninstallString" ) ).Returns( values );
+
+         // Act
+
+         var appCatalog = new AppCatalog( registryMock.Object );
+
+         var results = appCatalog.Find( appName );
+
+         // Assert
+
+         results.Should().HaveCount( 1 );
+         results[0].Name.Should().Be( appName );
       }
    }
 }
--- Runway/Runway/Commands/Uninstall/AppCatalog.cs
+++ Runway/Runway/Commands/Uninstall/AppCatalog.cs
@@ -1,12 +1,35 @@
-﻿namespace Runway.Commands.Uninstall
+﻿using System.Collections.Generic;
+
+namespace Runway.Commands.Uninstall
 {
    public class AppCatalog : IAppCatalog
    {
       public static AppEntry[] EmptyResults = new AppEntry[0];
 
+      private readonly IRegistry _registry;
+
+      public AppCatalog( IRegistry registry )
+      {
+         _registry = registry;
+      }
+
       public AppEntry[] Find( string name )
       {
-         return EmptyResults;
+         const string uninstallKeyName = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+
+         var appEntries = new List<AppEntry>();
+         var subKeys = _registry.GetSubKeysFromLocalMachine( uninstallKeyName );
+
+         foreach ( string subKey in subKeys )
+         {
+            var values = _registry.GetValuesFromLocalMachine( subKey, "DisplayName", "UninstallString" );
+
+            var appEntry = new AppEntry( values.Item1, values.Item2 );
+
+            appEntries.Add( appEntry );
+         }
+         
+         return appEntries.ToArray();
       }
    }
 }
