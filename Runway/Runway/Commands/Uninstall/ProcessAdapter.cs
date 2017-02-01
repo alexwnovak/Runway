e2eac1111367beed10f0849da3aa4da97f87a057
Alex Novak <alexwnovak@gmail.com>
2017-01-31 21:54:30 -0500

    +ProcessAdapter blank concrete

---
+++ Runway/Runway/Commands/Uninstall/ProcessAdapter.cs
@@ -0,0 +1,10 @@
+﻿namespace Runway.Commands.Uninstall
+{
+   public class ProcessAdapter : IProcess
+   {
+      public void Start( string path )
+      {
+         throw new System.NotImplementedException();
+      }
+   }
+}
