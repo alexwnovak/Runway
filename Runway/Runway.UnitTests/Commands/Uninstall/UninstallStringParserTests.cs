247894cd0d6a40aa6a49a5c783b14ea564763c0b
Alex Novak <alexwnovak@gmail.com>
2017-02-01 18:30:19 -0500

    +Case for string-closed, no arguments

--- Runway/Runway.UnitTests/Commands/Uninstall/UninstallStringParserTests.cs
+++ Runway/Runway.UnitTests/Commands/Uninstall/UninstallStringParserTests.cs
@@ -16,5 +16,17 @@ namespace Runway.UnitTests.Commands.Uninstall
          result.Item1.Should().Be( command );
          result.Item2.Should().BeEmpty();
       }
+
+      [Fact]
+      public void Parse_CommandEnclosedInQuotesButHasNoArgument_GetsTheCommandWithNoQuotes()
+      {
+         const string command = @"C:\Program Files (x86)\Audacity\unins000.exe";
+         string wholeLine = $@"""{command}""";
+
+         var result = UninstallStringParser.Parse( wholeLine );
+
+         result.Item1.Should().Be( command );
+         result.Item2.Should().BeEmpty();
+      }
    }
 }
--- Runway/Runway/Commands/Uninstall/UninstallStringParser.cs
+++ Runway/Runway/Commands/Uninstall/UninstallStringParser.cs
@@ -6,6 +6,12 @@ namespace Runway.Commands.Uninstall
    {
       public static Tuple<string, string> Parse( string wholeLine )
       {
+         if ( wholeLine.StartsWith( "\"" ) )
+         {
+            int nextQuoteIndex = wholeLine.IndexOf( '"', 1 );
+            wholeLine = wholeLine.Substring( 1, nextQuoteIndex - 1 );
+         }
+
          return new Tuple<string, string>( wholeLine, string.Empty );
       }
    }
