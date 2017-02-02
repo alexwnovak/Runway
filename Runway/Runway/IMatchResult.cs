181bd06a26c906108417130ad211148e03bf56da
Alex Novak <alexwnovak@gmail.com>
2017-02-02 18:07:34 -0500

    +IMatchResult
    This so far is an extraction over MatchResult, so we can start using it without changing any behavior or tests. This will become an abstraction for various types of match results, either from the app catalog, or from a specific command supplying suggestions for arguments.

---
+++ Runway/Runway/IMatchResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Runway
+{
+   public interface IMatchResult
+   {
+      MatchType MatchType
+      {
+         get;
+      }
+
+      ILaunchableCommand Command
+      {
+         get;
+      }
+   }
+}
