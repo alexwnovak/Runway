282f1d49d948f5bef7ac69248ef64a9dfeac25fc
Alex Novak <alexwnovak@gmail.com>
2017-02-07 22:52:13 -0500

    +AppMatchResult
    We'll use this to return app info from searching the app catalog. This doesn't return commands, but instead different kinds of data.

---
+++ Runway/Runway/Commands/Uninstall/AppMatchResult.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Runway.Input;
+
+namespace Runway.Commands.Uninstall
+{
+   public class AppMatchResult : IMatchResult
+   {
+      public MatchType MatchType
+      {
+         get;
+      }
+
+      public string DisplayText
+      {
+         get;
+      }
+
+      public ISearchCatalog Source
+      {
+         get;
+      }
+
+      public AppMatchResult( string displayText )
+      {
+         DisplayText = displayText;
+      }
+
+      public IInputFrame BeginInputFrame()
+      {
+         throw new NotImplementedException();
+      }
+
+      public void Activate( object[] parameters )
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
