fe09a95a4aad2bf025adb707d63fed4895d99ab5
Alex Novak <alexwnovak@gmail.com>
2017-02-07 22:48:24 -0500

    +AppSearchCatalog blank class and test class
    This will be what the existing AppCatalog turns into; that currently is its own thing, however it's BASICALLY an ISearchCatalog. Now it has to actually become one to jive with the input frame system.

---
+++ Runway/Runway/Commands/Uninstall/AppSearchCatalog.cs
@@ -0,0 +1,10 @@
+﻿namespace Runway.Commands.Uninstall
+{
+   public class AppSearchCatalog : ISearchCatalog
+   {
+      public IMatchResult[] Search( string searchText )
+      {
+         throw new System.NotImplementedException();
+      }
+   }
+}
