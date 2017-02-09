d05b78131dd5999d5502b929a23afa0a8d9b27ef
Alex Novak <alexwnovak@gmail.com>
2017-02-08 22:35:17 -0500

    AppSearchCatalog now implements this new interface

--- Runway/Runway/Commands/Uninstall/AppSearchCatalog.cs
+++ Runway/Runway/Commands/Uninstall/AppSearchCatalog.cs
@@ -1,6 +1,10 @@
-﻿namespace Runway.Commands.Uninstall
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Runway.Commands.Uninstall
 {
-   public class AppSearchCatalog : ISearchCatalog
+   public class AppSearchCatalog : IAppSearchCatalog
    {
       private readonly IRegistry _registry;
 
@@ -12,6 +16,11 @@
       public IMatchResult[] Search( string searchText )
       {
          throw new System.NotImplementedException();
+      }
+
+      public void Uninstall( string path )
+      {
+         throw new NotImplementedException();
       }
    }
 }
