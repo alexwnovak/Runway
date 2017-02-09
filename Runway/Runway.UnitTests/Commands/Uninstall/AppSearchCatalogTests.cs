54d83c1e183c183a9ee1c3fc97ef929a0ba862a8
Alex Novak <alexwnovak@gmail.com>
2017-02-08 22:41:34 -0500

    Moving the AppCatalog smarts/tests into AppSearchCatalog

--- Runway/Runway.UnitTests/Commands/Uninstall/AppSearchCatalogTests.cs
+++ Runway/Runway.UnitTests/Commands/Uninstall/AppSearchCatalogTests.cs
@@ -1,6 +1,139 @@
-﻿namespace Runway.UnitTests.Commands.Uninstall
+﻿using Xunit;
+using FluentAssertions;
+using Moq;
+using Runway.Commands.Uninstall;
+using Runway.UnitTests.Helpers;
+
+namespace Runway.UnitTests.Commands.Uninstall
 {
    public class AppSearchCatalogTests
    {
+      [Fact]
+      public void Find_CatalogIsEmpty_ReturnsNoResults()
+      {
+         // Arrange
+
+         var registryMock = new Mock<IRegistry>();
+
+         // Act
+
+         var appCatalog = new AppSearchCatalog( registryMock.Object, null );
+
+         var results = appCatalog.Search( "notepad" );
+
+         // Assert
+
+         results.Should().HaveCount( 0 );
+      }
+
+      [Fact]
+      public void Find_SearchesForAppByName_FindsApp()
+      {
+         const string appName = "notepad";
+         const string path = "subKeyPath";
+
+         // Arrange
+
+         var registryResults = ArrayHelper.Create( path );
+
+         var registryMock = new Mock<IRegistry>();
+         registryMock.Setup( r => r.GetSubKeysFromLocalMachine( It.IsAny<string>() ) ).Returns( registryResults );
+
+         registryMock.Setup( r => r.GetValueFromLocalMachine( path, "DisplayName" ) ).Returns( appName );
+
+         // Act
+
+         var appCatalog = new AppSearchCatalog( registryMock.Object, null );
+
+         var results = appCatalog.Search( appName );
+
+         // Assert
+
+         results.Should().HaveCount( 1 );
+         results[0].DisplayText.Should().Be( appName );
+         //results[0].Path.Should().EndWith( path );
+      }
+
+      [Fact]
+      public void Find_HasOneAppAndSearchesForSomethingElse_ReturnsAnEmptySet()
+      {
+         const string appName = "notepad";
+         const string path = "subKeyPath";
+
+         // Arrange
+
+         var registryResults = ArrayHelper.Create( path );
+
+         var registryMock = new Mock<IRegistry>();
+         registryMock.Setup( r => r.GetSubKeysFromLocalMachine( It.IsAny<string>() ) ).Returns( registryResults );
+
+         registryMock.Setup( r => r.GetValueFromLocalMachine( path, "DisplayName" ) ).Returns( appName );
+
+         // Act
+
+         var appCatalog = new AppSearchCatalog( registryMock.Object, null );
+
+         var results = appCatalog.Search( "doesnotexist" );
+
+         // Assert
+
+         results.Should().HaveCount( 0 );
+      }
+
+      [Fact]
+      public void Find_HasTwoAppsAndSearchesWithPartialText_ReturnsBoth()
+      {
+         const string appName1 = "notepad";
+         const string path1 = "subKeyPath";
+
+         const string appName2 = "notepad++";
+         const string path2 = "subKeyPath2";
+
+         // Arrange
+
+         var registryResults = ArrayHelper.Create( path1, path2 );
+
+         var registryMock = new Mock<IRegistry>();
+         registryMock.Setup( r => r.GetSubKeysFromLocalMachine( It.IsAny<string>() ) ).Returns( registryResults );
+
+         registryMock.Setup( r => r.GetValueFromLocalMachine( path1, "DisplayName" ) ).Returns( appName1 );
+         registryMock.Setup( r => r.GetValueFromLocalMachine( path2, "DisplayName" ) ).Returns( appName2 );
+
+         // Act
+
+         var appCatalog = new AppSearchCatalog( registryMock.Object, null );
+
+         var results = appCatalog.Search( "n" );
+
+         // Assert
+
+         results.Should().HaveCount( 2 );
+      }
+
+      [Fact]
+      public void Uninstall_PassesAppPath_StartsCorrespondingUninstallString()
+      {
+         const string path = "path";
+         const string uninstallCommand = @"C:\Program Files\Notepad++\Notepad++.exe";
+         const string uninstallArgument = "/uninstall";
+         string uninstallString = $"\"{uninstallCommand}\" {uninstallArgument}";
+
+         // Arrange
+
+         var processMock = new Mock<IProcess>();
+
+         var registryMock = new Mock<IRegistry>();
+         registryMock.Setup( r => r.GetValueFromLocalMachine( path, "UninstallString" ) ).Returns( uninstallString );
+
+         // Act
+
+         var appCatalog = new AppSearchCatalog( registryMock.Object, processMock.Object );
+
+         appCatalog.Uninstall( path );
+
+         // Assert
+
+         processMock.Verify( p => p.Start( uninstallCommand, uninstallArgument ), Times.Once() );
+      }
    }
 }
--- Runway/Runway/Commands/Uninstall/AppSearchCatalog.cs
+++ Runway/Runway/Commands/Uninstall/AppSearchCatalog.cs
@@ -7,20 +7,43 @@ namespace Runway.Commands.Uninstall
    public class AppSearchCatalog : IAppSearchCatalog
    {
       private readonly IRegistry _registry;
+      private readonly IProcess _process;
 
-      public AppSearchCatalog( IRegistry registry )
+      public AppSearchCatalog( IRegistry registry, IProcess process )
       {
          _registry = registry;
+         _process = process;
       }
 
       public IMatchResult[] Search( string searchText )
       {
-         throw new System.NotImplementedException();
+         const string uninstallKeyName = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+
+         var appEntries = new List<AppEntry>();
+         var subKeys = _registry.GetSubKeysFromLocalMachine( uninstallKeyName );
+
+         foreach ( string subKey in subKeys )
+         {
+            string displayName = _registry.GetValueFromLocalMachine( subKey, "DisplayName" );
+
+            if ( !string.IsNullOrEmpty( displayName ) && displayName.StartsWith( searchText, StringComparison.InvariantCultureIgnoreCase ) )
+            {
+               var appEntry = new AppEntry( displayName, subKey );
+
+               appEntries.Add( appEntry );
+            }
+         }
+
+         return appEntries.Select( ae => new AppMatchResult( ae.Name ) ).ToArray();
       }
 
       public void Uninstall( string path )
       {
-         throw new NotImplementedException();
+         string uninstallString = _registry.GetValueFromLocalMachine( path, "UninstallString" );
+
+         var parsedUninstallString = UninstallStringParser.Parse( uninstallString );
+
+         _process.Start( parsedUninstallString.Item1, parsedUninstallString.Item2 );
       }
    }
 }
