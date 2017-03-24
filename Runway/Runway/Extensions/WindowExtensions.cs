9063cf99b4e7b5682ff0e12bd41acfa33863d214
Alex Novak <alexwnovak@gmail.com>
2017-03-23 20:55:35 -0400

    +WindowExtensions class for fading in/out the window

---
+++ Runway/Runway/Extensions/WindowExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace Runway.Extensions
+{
+   public static class WindowExtensions
+   {
+      private const double _fadeDuration = 100;
+
+      public static void FadeOut( this Window window )
+      {
+         var doubleAnimation = new DoubleAnimation( 1, 0, new Duration( TimeSpan.FromMilliseconds( _fadeDuration ) ) );
+         
+         doubleAnimation.Completed += ( _, __ ) => window.Visibility = Visibility.Hidden;
+
+         window.BeginAnimation( UIElement.OpacityProperty, doubleAnimation );
+      }
+
+      public static void FadeIn( this Window window )
+      {
+         var doubleAnimation = new DoubleAnimation( 0, 1, new Duration( TimeSpan.FromMilliseconds( _fadeDuration ) ) );
+         
+         doubleAnimation.Completed += ( _, __ ) => window.Activate();
+
+         window.BeginAnimation( UIElement.OpacityProperty, doubleAnimation );
+      }
+   }
+}
