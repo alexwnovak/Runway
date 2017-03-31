7eee5baf779bffa1bd3588c37c637c28d4a92edd
Alex Novak <alexwnovak@gmail.com>
2017-03-30 22:16:06 -0400

    +Event args for when a key is intercepted
    Also provides a way to indicate that we should suppress/handle it and not pass it along to the system.

---
+++ Runway/Runway/Input/KeyboardHookEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Runway.Input
+{
+   public class KeyboardHookEventArgs : EventArgs
+   {
+      public int KeyCode
+      {
+         get;
+      }
+
+      public bool Handled
+      {
+         get;
+         set;
+      }
+
+      public KeyboardHookEventArgs( int keyCode )
+      {
+         KeyCode = keyCode;
+      }
+   }
+}
