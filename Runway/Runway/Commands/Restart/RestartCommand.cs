074fe8ff3a5bd11fe964ccb0f29b1cf955d0812f
Alex Novak <alexwnovak@gmail.com>
2017-04-02 21:54:50 -0400

    +RestartCommand blank class

---
+++ Runway/Runway/Commands/Restart/RestartCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Runway.Commands.Restart
+{
+   public class RestartCommand : LaunchableCommandBase
+   {
+      public RestartCommand( string commandText )
+         : base( commandText )
+      {
+      }
+
+      public override void Launch( object[] parameters )
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
