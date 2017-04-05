2f5e286fc2e51ed6715d2a0d8aab0ce272e5a9b1
Alex Novak <alexwnovak@gmail.com>
2017-04-04 20:08:47 -0400

    Now binds to the icon provided by the command base

--- Runway/Runway/LaunchableCommandBase.cs
+++ Runway/Runway/LaunchableCommandBase.cs
@@ -1,4 +1,8 @@
-﻿namespace Runway
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Runway
 {
    public abstract class LaunchableCommandBase : ILaunchableCommand
    {
@@ -12,10 +16,18 @@
          get;
       }
 
+      public ImageSource Icon
+      {
+         get;
+         protected set;
+      }
+
       protected LaunchableCommandBase( string commandText, string description )
       {
          CommandText = commandText;
          Description = description;
+
+         Icon = new BitmapImage( new Uri( "/Resources/app.png", UriKind.Relative ) );
       }
 
       public abstract void Launch( object[] parameters );
