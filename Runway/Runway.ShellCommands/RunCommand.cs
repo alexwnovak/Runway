7050abf5c0c15d51fbb64b913da059c77f1cfb6f
Alex Novak <alexwnovak@gmail.com>
2017-04-06 19:09:06 -0400

    +Run command

---
+++ Runway/Runway.ShellCommands/RunCommand.cs
@@ -0,0 +1,17 @@
+﻿using Runway.ExtensibilityModel;
+using Shell32;
+
+namespace Runway.ShellCommands
+{
+   public class RunCommand : ILaunchableCommand
+   {
+      public string CommandText => "run";
+      public string Description => "Launches the Windows Run dialog"; 
+
+      public void Launch( object[] parameters )
+      {
+         var shell = new Shell();
+         shell.FileRun();
+      }
+   }
+}
