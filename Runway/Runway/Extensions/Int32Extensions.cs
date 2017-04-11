bc99dd4d02726d21e76e796c991f7131af61d2d2
Alex Novak <alexwnovak@gmail.com>
2017-04-10 22:52:05 -0400

    +Increment and Decrement extensions for range-clamping

--- Runway/Runway/Extensions/Int32Extensions.cs
+++ Runway/Runway/Extensions/Int32Extensions.cs
@@ -2,6 +2,7 @@
 {
    public static class Int32Extensions
    {
-
+      public static int Increment( this int value, int max ) => ++value >= max ? 0 : value;
+      public static int Decrement( this int value, int reset ) => --value < 0 ? reset : value;
    }
 }
