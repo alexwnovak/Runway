747851a6a3a4f13000a036d6091980ef8c92e348
Alex Novak <alexwnovak@gmail.com>
2017-04-13 22:58:24 -0400

    +StopCommand blank command

---
+++ Runway/Runway/Commands/Stop/StopCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Runway.ExtensibilityModel;
+
+namespace Runway.Commands.Stop
+{
+   public class StopCommand : IQueryableCommand
+   {
+      public string CommandText
+      {
+         get;
+      }
+
+      public string Description
+      {
+         get;
+      }
+
+      public void Launch()
+      {
+         throw new NotImplementedException();
+      }
+
+      public ISearchCatalog Query()
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
