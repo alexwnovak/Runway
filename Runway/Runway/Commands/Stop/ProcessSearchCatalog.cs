cadd79cb65e765bccee3982c919947704cbed023
Alex Novak <alexwnovak@gmail.com>
2017-04-13 22:59:44 -0400

    +ProcessSearchCatalog empty catalog
    This will be used to find/return running processes.

---
+++ Runway/Runway/Commands/Stop/ProcessSearchCatalog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Runway.ExtensibilityModel;
+
+namespace Runway.Commands.Stop
+{
+   public class ProcessSearchCatalog : ISearchCatalog
+   {
+      public IMatchResult[] Search( string searchText )
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
