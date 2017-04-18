cd231f2c87cf2390ba9b0839b410a611de1f5069
Alex Novak <alexwnovak@gmail.com>
2017-04-17 22:53:11 -0400

    FadeBehavior now fades in when the window is activated

--- Runway/Runway/Behaviors/WindowFadeBehavior.cs
+++ Runway/Runway/Behaviors/WindowFadeBehavior.cs
@@ -1,9 +1,20 @@
+﻿using System;
 using System.Windows;
 using System.Windows.Interactivity;
+using Runway.Extensions;
 
 namespace Runway.Behaviors
 {
    public class WindowFadeBehavior : Behavior<Window>
    {
+      protected override void OnAttached() => AssociatedObject.Activated += OnActivated;
+      protected override void OnDetaching() => AssociatedObject.Activated -= OnActivated;
+
+      private void OnActivated( object sender, EventArgs e )
+      {
+         AssociatedObject.Opacity = 0;
+         AssociatedObject.Visibility = Visibility.Visible;
+         AssociatedObject.FadeIn();
+      }
    }
 }
