405d40503c28c2ec42557c3f0df8f419ac2e5284
Alex Novak <alexwnovak@gmail.com>
2017-04-18 18:55:08 -0400

    +WindowCursorBehavior to hide the cursor on window activate
    Doesn't yet restore the cursor on any kind of activity.

---
+++ Runway/Runway/Behaviors/WindowCursorBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace Runway.Behaviors
+{
+   public class WindowCursorBehavior : Behavior<Window>
+   {
+      [DllImport( "user32.dll" )]
+      private static extern int ShowCursor( bool bShow );
+
+      protected override void OnAttached() => AssociatedObject.Activated += OnActivated;
+
+      private void OnActivated( object sender, EventArgs e )
+      {
+         ShowCursor( false );
+      }
+   }
+}
